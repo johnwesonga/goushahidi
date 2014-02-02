544d00e96e2924bd800fb9eca0b580f90f85eaab
John Wesonga <johnwesonga@gmail.com>
2014-02-03 02:52:58 +0300

    changed repo structure and started on UsersService.

--- tags.go
+++
@@ -1,8 +0,0 @@
-package goushahidi
-
-// TagsService provides access to the Tags Resource
-//
-// Ushahidi v3 API docs: https://wiki.ushahidi.com/display/WIKI/Ushahidi+3.x+API+Tags+Resource
-type TagsService struct {
-	client *Client
-}
--- users.go
+++
@@ -1,8 +0,0 @@
-package goushahidi
-
-// UsersService provides access to the Users Resource
-//
-// Ushahidi v3 API docs: https://wiki.ushahidi.com/display/WIKI/Ushahidi+3.x+API+Users+Resource
-type UsersService struct {
-	client *Client
-}
