66a1227ff6ff3b547627a8366c56f6f0fde2cecc
John Wesonga <johnwesonga@gmail.com>
2014-02-03 00:34:39 +0300

    added skeleton for users, tags and posts services.

---
+++ posts.go
@@ -0,0 +1,8 @@
+package goushahidi
+
+// UsersService provides access to the Posts Resource
+//
+// Ushahidi v3 API docs: https://wiki.ushahidi.com/display/WIKI/Ushahidi+3.x+API+Posts+Resource
+type PostsService struct {
+	client *Client
+}
---
+++ users.go
@@ -0,0 +1,8 @@
+package goushahidi
+
+// UsersService provides access to the Users Resource
+//
+// Ushahidi v3 API docs: https://wiki.ushahidi.com/display/WIKI/Ushahidi+3.x+API+Users+Resource
+type UsersService struct {
+	client *Client
+}
