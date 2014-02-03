5d665b0fe1cc958267d657bdcffd445838290521
John Wesonga <johnwesonga@gmail.com>
2014-02-03 22:57:55 +0300

    updating examples code.

--- examples/example.go
+++ examples/example.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/johnwesonga/goushahidi/ushahidi"
+)
+
+const (
+	ushahidiInstance = "http://myushahidi.com"
 )
 
 func main() {
+	client := client.NewClient(ushahidiInstance, nil)
 	fmt.Println("Ushahidi Examples Here")
 
 }
